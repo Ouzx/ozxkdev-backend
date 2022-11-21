f915483d73da1b63f952931fd01d0325e3780513
Oz <oguzhankandakoglu@gmail.com>
2022-11-22 02:34:27 +0300

    PostBox

--- inhalt-cms/src/Components/Posts/Posts.tsx
+++ inhalt-cms/src/Components/Posts/Posts.tsx
@@ -1,16 +1,23 @@
 import { useState } from "react";
 import CreateNewButton from "../Header/CreateNewButton";
+import PostBox from "./Post/PostBox";
 
 const Posts = () => {
-  const [posts, setPosts] = useState([]);
+  const [posts, setPosts] = useState(["a", "b", "c", "d", "e", "f", "g", "h"]);
   return (
     <div className="flex items-center justify-center">
-      <div className="flex flex-col flex-1 justify-start max-w-2xl bg-gray-100 p-3 ">
+      <div className="flex flex-col flex-1 justify-start max-w-2xl bg-gray-100 p-6 ">
         <p className="text-2xl mb-12">Posts</p>
-        {posts.length <= 0 && (
+        {posts.length <= 0 ? (
           <div className="space-y-9">
             <p>Nothing found! Create new Instead</p>
             <CreateNewButton />
+          </div>
+        ) : (
+          <div className="space-y-6">
+            {posts.map((post) => (
+              <PostBox key={post} />
+            ))}
           </div>
         )}
       </div>
