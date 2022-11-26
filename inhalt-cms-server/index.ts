485ed298ffe662237f4075973a07130c74996a08
Oz <oguzhankandakoglu@gmail.com>
2022-11-26 22:54:16 +0300

    ENV

--- inhalt-cms-server/index.ts
+++ inhalt-cms-server/index.ts
@@ -1,10 +1,20 @@
 import express, { Express, Request, Response } from "express";
-import dotenv from "dotenv";
+import mongoose from "mongoose";
+import cors from "cors";
 
+import dotenv from "dotenv";
 dotenv.config();
+
+import postRoutes from "./routes/posts.js";
 
 const app: Express = express();
 const port = process.env.PORT;
+
+app.use(express.json({ limit: "30mb" }));
+app.use(express.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+app.use("/posts", postRoutes);
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Express + TypeScript Server");
