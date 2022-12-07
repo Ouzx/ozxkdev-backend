b3c71590b1c2e15333e2addf438fb5a07231b345
Oz <oguzhankandakoglu@gmail.com>
2022-12-07 22:18:42 +0300

    cleanin up

--- inhalt-cms/src/components/Navbar/Navbar.tsx
+++ inhalt-cms/src/components/Navbar/Navbar.tsx
@@ -1,10 +1,7 @@
-import React from "react";
 import CreateNewButton from "./CreateNewButton";
 import SearchButton from "./SearchButton";
-import { useNavigate } from "react-router-dom";
 import Logo from "./Logo";
 const Navbar = () => {
-  const navigate = useNavigate();
   return (
     <nav className="flex items-center justify-center drop-shadow-xl bg-white mb-32">
       <div className=" flex flex-1 justify-between items-center px-4 py-6 max-w-6xl ">
