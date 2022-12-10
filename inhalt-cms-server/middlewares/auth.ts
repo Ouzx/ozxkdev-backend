8a86ea6c0d28060536f765126245083e9262ab74
Oz <oguzhankandakoglu@gmail.com>
2022-12-10 21:11:11 +0300

    middleware for auth

---
+++ inhalt-cms-server/middlewares/auth.ts
@@ -0,0 +1,29 @@
+import jwt from "jsonwebtoken";
+import { Request, Response, NextFunction } from "express";
+
+export const verifyToken = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    let token = req.header("Authorization");
+
+    if (!token) return res.status(401).json({ message: "Unauthorized" });
+
+    if (token.startsWith("Bearer "))
+      token = token.slice(7, token.length).trimLeft();
+
+    const secret = process.env.JWT_SECRET as string;
+    if (!secret) throw new Error("JWT_SECRET is not defined");
+
+    const decoded = jwt.verify(token, secret);
+    req.body.user = decoded;
+
+    next();
+  } catch (error) {
+    if (error instanceof Error) {
+      res.status(409).json({ message: error.message });
+    } else res.status(500).json({ message: "Something went wrong" });
+  }
+};
