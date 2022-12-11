1c6b8a56182c9a9d89edcd955337072c6f4acee7
Oz <oguzhankandakoglu@gmail.com>
2022-12-11 19:23:23 +0300

    inputbox maded general component

---
+++ inhalt-cms/src/components/InputBox.tsx
@@ -0,0 +1,29 @@
+import React, { useState, Ref } from "react";
+
+interface Props {
+  title: string;
+  value?: string;
+  id?: string;
+  type?: string;
+}
+
+const InputBox = React.forwardRef(
+  (props: Props, ref: Ref<HTMLInputElement>) => {
+    return (
+      <div>
+        <label htmlFor={props.id}>{props.title}</label>
+        <input
+          type={props.type || "text"}
+          id={props.id}
+          name={props.id}
+          ref={ref}
+          defaultValue={props.value}
+          required
+          className="p-1 pl-2 border border-gray-400 rounded-md"
+        />
+      </div>
+    );
+  }
+);
+
+export default InputBox;
