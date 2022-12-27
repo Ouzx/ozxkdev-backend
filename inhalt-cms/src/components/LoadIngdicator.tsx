76bce1ceddeb6d960eb43afca891c85ab8c26444
Ouzx <oguzhankandakoglu@gmail.com>
2022-12-27 23:51:05 +0300

    loading indicator updated

--- inhalt-cms/src/components/LoadIngdicator.tsx
+++ inhalt-cms/src/components/LoadIngdicator.tsx
@@ -3,12 +3,13 @@ import { InfinitySpin } from "react-loader-spinner";
 const LoadIngdicator: React.FC<{
   color?: string;
   backgroundColor?: string;
-}> = ({ color = "black", backgroundColor = "gray-100" }) => {
+  width?: string;
+}> = ({ color = "black", backgroundColor = "gray-100", width = "200" }) => {
   return (
     <div
-      className={`flex flex-1 self-center items-center justify-center bg-${backgroundColor}`}
+      className={`flex flex-1 self-center items-center justify-center ${backgroundColor}`}
     >
-      <InfinitySpin color={color} />
+      <InfinitySpin color={color} width={width} />
     </div>
   );
 };
