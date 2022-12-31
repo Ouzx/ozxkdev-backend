b9baf364e2ecabcbcb10401433d5ec3fdb5f23d1
Ouzx <oguzhankandakoglu@gmail.com>
2022-12-31 14:24:40 +0300

    404 page

---
+++ inhalt-cms/src/pages/FourOhFour.tsx
@@ -0,0 +1,30 @@
+import { useNavigate } from "react-router-dom";
+import svgImg from "../assets/cat";
+
+const FourOhFour = () => {
+  const navigate = useNavigate();
+  return (
+    <div className="h-screen w-screen flex items-center">
+      <div className="container flex flex-col md:flex-row items-center justify-center px-5 text-gray-700">
+        <div className="max-w-md">
+          <div className="text-5xl font-dark font-bold">404</div>
+          <p className="text-2xl md:text-3xl font-light leading-normal">
+            Sorry I couldn't find this page.{" "}
+          </p>
+          <p className="mb-8">
+            But dont worry, you can find plenty of other things on my homepage.
+          </p>
+          <button
+            onClick={() => navigate("/")}
+            className="px-4 inline py-2 text-sm font-medium leading-5 shadow text-white transition-colors duration-150 border border-transparent rounded-lg focus:outline-none focus:shadow-outline-purple bg-purple-600 active:bg-purple-600 hover:bg-purple-700"
+          >
+            Back to homepage
+          </button>
+        </div>
+        <div className="max-w-lg">{svgImg}</div>
+      </div>
+    </div>
+  );
+};
+
+export default FourOhFour;
--- inhalt-cms/src/pages/index.ts
+++ inhalt-cms/src/pages/index.ts
@@ -2,5 +2,6 @@ import Login from "./Login";
 import Post from "./Post";
 import Search from "./Search";
 import Home from "./Home";
+import FourOhFour from "./FourOhFour";
 
-export { Login, Post, Search, Home };
+export { Login, Post, Search, Home, FourOhFour };
