4f380dfd0265f0d52975f66fc5e072c93f8b1343
Ouzx <oguzhankandakoglu@gmail.com>
2022-12-31 06:56:15 +0300

    editor page removed

--- inhalt-cms/src/pages/index.ts
+++ inhalt-cms/src/pages/index.ts
@@ -2,6 +2,5 @@ import Login from "./Login";
 import Post from "./Post";
 import Search from "./Search";
 import Home from "./Home";
-import EditorPage from "./EditorPage";
 
-export { Login, Post, Search, Home, EditorPage };
+export { Login, Post, Search, Home };
