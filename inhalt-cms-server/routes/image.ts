ca54e1d1ac0f57ef9d2c4b0e44b8c280a7ba751c
Ouzx <oguzhankandakoglu@gmail.com>
2023-02-11 23:10:56 +0300

    by-url disabled

--- inhalt-cms-server/routes/image.ts
+++ inhalt-cms-server/routes/image.ts
@@ -6,6 +6,6 @@ import { byFile, byURL } from "../controllers/image.js";
 const router = Router();
 
 router.post("/by-file", multerMiddlewareSingle, byFile);
-router.post("/by-url", byURL);
+// router.post("/by-url", byURL);
 
 export default router;
