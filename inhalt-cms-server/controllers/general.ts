265363e0bc4a5f90ec59c32cdb4d5729ec722d77
Ouzx <oguzhankandakoglu@gmail.com>
2023-02-12 19:27:43 +0300

    General controller optimisation

--- inhalt-cms-server/controllers/general.ts
+++ inhalt-cms-server/controllers/general.ts
@@ -1,6 +1,7 @@
-import { Types } from "mongoose";
 import { Request, Response } from "express";
 import Post, { iPost } from "../models/post.js";
+
+const GENERAL_SELECTOR = "-rawContent -_id -__v";
 
 export const getPosts = async (req: Request, res: Response) => {
   const { category, pageIndex } = req.params;
@@ -15,24 +16,13 @@ export const getPosts = async (req: Request, res: Response) => {
     let totalItems: number = 0;
 
     let posts: iPost[] = [];
-    // TODO: Make category case insensitive
-    if (category.toLowerCase() === "all") {
-      totalItems = await Post.find().countDocuments();
-      posts = await Post.find()
-        .sort({ createdAt: -1 })
-        .skip((+page - 1) * ITEMS_PER_PAGE)
-        .limit(ITEMS_PER_PAGE);
-    } else {
-      totalItems = await Post.find({ category: category }).countDocuments();
-      posts = await Post.find({ category: category })
-        .sort({ createdAt: -1 })
-        .skip((+page - 1) * ITEMS_PER_PAGE)
-        .limit(ITEMS_PER_PAGE);
-    }
-    posts.forEach((post) => {
-      post.content = "";
-      post.raw = "";
-    });
+    posts = await (category.toLowerCase() === "all"
+      ? Post.find().select(GENERAL_SELECTOR)
+      : Post.find({ category: category }).select(GENERAL_SELECTOR)
+    )
+      .sort({ createdAt: -1 })
+      .skip((+page - 1) * ITEMS_PER_PAGE)
+      .limit(ITEMS_PER_PAGE);
 
     res.status(200).json({ posts, totalItems });
   } catch (e) {
@@ -48,38 +38,29 @@ export const getPost = async (req: Request, res: Response) => {
   try {
     if (!slug) throw new Error(`No post with slug: ${slug}`);
 
-    const post = await Post.find({ urlSuffix: slug });
+    const post = await Post.find({ slug }).select("-__v -_id");
+
     const previousPost = await Post.findOne({
       createdAt: { $lt: post[0].createdAt },
-    });
+    }).select("slug");
 
     const nextPost = await Post.findOne({
       createdAt: { $gt: post[0].createdAt },
-    });
+    }).select("slug");
 
     const relatedPosts = await Post.find({
       category: post[0].category,
       _id: { $ne: post[0]._id },
     })
+      .select(GENERAL_SELECTOR)
       .sort({ createdAt: -1 })
       .limit(3);
 
-    post.forEach((post) => {
-      post.raw = "";
-    });
-
-    relatedPosts.forEach((post) => {
-      post.content = "";
-      post.raw = "";
-    });
-
-    const prevPost = previousPost?.urlSuffix;
-    const nexPost = nextPost?.urlSuffix;
     res.status(200).json({
       post: post[0],
       relatedPosts,
-      prevPost,
-      nexPost,
+      previousPost,
+      nextPost,
     });
   } catch (e) {
     if (e instanceof Error) {
@@ -88,27 +69,26 @@ export const getPost = async (req: Request, res: Response) => {
   }
 };
 
-// TODO: Move search term to body
 export const searchPosts = async (req: Request, res: Response) => {
-  const { searchTerm, pageIndex } = req.params;
-
+  const { pageIndex, searchTerm } = req.params;
   try {
     if (!searchTerm) throw new Error(`No search term: ${searchTerm}`);
     if (!pageIndex) throw new Error(`No page with id: ${pageIndex}`);
 
+    const decodedSearchTerm = decodeURIComponent(searchTerm);
+
     const page: number = +pageIndex + 1;
     const ITEMS_PER_PAGE = 5;
 
-    const totalItems = await Post.find().countDocuments();
+    const totalItems = await Post.find({
+      $text: { $search: decodedSearchTerm },
+    }).countDocuments();
 
-    const posts = await Post.find({ $text: { $search: searchTerm } })
+    const posts = await Post.find({ $text: { $search: decodedSearchTerm } })
+      .select(GENERAL_SELECTOR)
       .sort({ createdAt: -1 })
       .skip((+page - 1) * ITEMS_PER_PAGE)
       .limit(ITEMS_PER_PAGE);
-
-    posts.forEach((post) => {
-      post.content = "";
-    });
 
     res.status(200).json({ posts, totalItems });
   } catch (e) {
