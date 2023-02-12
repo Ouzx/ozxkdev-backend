84a0b2c3229220bfb04512d264c2b4b80e3a99c7
Ouzx <oguzhankandakoglu@gmail.com>
2023-02-12 18:32:51 +0300

    cache clean

--- inhalt-cms-server/dist/routes/image.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { multerMiddlewareSingle } from "../middlewares/imager.js";
-import { byFile } from "../controllers/image.js";
-const router = Router();
-router.post("/by-file", multerMiddlewareSingle, byFile);
-// router.post("/by-url", byURL);
-export default router;
-//# sourceMappingURL=image.js.map
--- inhalt-cms-server/routes/index.ts
+++
@@ -1,5 +0,0 @@
-import auth from "./auth.js";
-import image from "./image.js";
-import post from "./post.js";
-import general from "./general.js";
-export { auth, image, post, general };
