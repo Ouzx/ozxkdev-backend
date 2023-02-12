3dc20413835c272599a76e5c3fa867525911df52
Ouzx <oguzhankandakoglu@gmail.com>
2023-02-12 18:57:02 +0300

    by-url route removed

--- inhalt-cms-server/routes/image.ts
+++ inhalt-cms-server/routes/image.ts
@@ -1,11 +1,10 @@
 import { Router } from "express";
 import { multerMiddlewareSingle } from "../middlewares/imager.js";
 
-import { byFile, byURL } from "../controllers/image.js";
+import { byFile } from "../controllers/image.js";
 
 const router = Router();
 
 router.post("/by-file", multerMiddlewareSingle, byFile);
-// router.post("/by-url", byURL);
 
 export default router;
