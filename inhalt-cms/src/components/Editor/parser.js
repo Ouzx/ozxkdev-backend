5805878ff562d7c57e8427b2a47fa0bb0b18e3a7
Ouzx <oguzhankandakoglu@gmail.com>
2023-03-16 16:57:00 +0300

    folder replacement

--- inhalt-cms/src/components/Editor/parser.js
+++
@@ -1,4 +0,0 @@
-import edjsParser from "@herii/editorjs-parser";
-
-const parser = new edjsParser();
-export const parse = (editorjs_data) => parser.parse(editorjs_data);
--- inhalt-cms/src/pages/index.ts
+++
@@ -1,8 +0,0 @@
-import Login from "./Login";
-import Post from "./Post";
-import Search from "./Search";
-import Home from "./Home";
-import FourOhFour from "./FourOhFour";
-import Welcome from "./Welcome";
-
-export { Login, Post, Search, Home, FourOhFour, Welcome };
