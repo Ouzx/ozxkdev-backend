311eb09b9526e5bbe8a043115e650346b7992d4c
Ouzx <oguzhankandakoglu@gmail.com>
2023-03-19 20:07:53 +0300

    serverless disabled

--- dist/index.js
+++ dist/index.js
@@ -53,5 +53,5 @@ mongoose
 })
     .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
     .catch((error) => console.log(`${error} did not connect`));
-export default app;
+// export default app;
 //# sourceMappingURL=index.js.map
