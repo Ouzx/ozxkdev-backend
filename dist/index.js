6b1bd35519664b0fa8622d2c876b36f9b4128825
Ouzx <oguzhankandakoglu@gmail.com>
2023-08-09 02:01:13 +0300

    cors config

--- dist/index.js
+++ dist/index.js
@@ -24,6 +24,7 @@ app.use(cors({
         "http://localhost:3000",
         "http://127.0.0.1:3000",
         "https://ozxk.dev",
+        "http://ozxk.dev",
     ],
 }));
 /* Set up rate limiter */
--- index.ts
+++ index.ts
@@ -32,6 +32,7 @@ app.use(
 			"http://localhost:3000",
 			"http://127.0.0.1:3000",
 			"https://ozxk.dev",
+			"http://ozxk.dev",
 		],
 	})
 );
